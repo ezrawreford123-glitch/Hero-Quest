4564cf2b1fdf428748515e0d6ecfa714f4a1c089
Ezra Wreford <ezrawref402@ed.amdsb.ca>
2025-09-26 10:31:42 -0400

    Add project files.

---
+++ Hero Quest/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Hero_Quest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.nameButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.Color.White;
+            this.nameLabel.Location = new System.Drawing.Point(25, 55);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(205, 36);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Enter your name:";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInput.Location = new System.Drawing.Point(236, 57);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(157, 34);
+            this.nameInput.TabIndex = 1;
+            this.nameInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // nameButton
+            // 
+            this.nameButton.BackColor = System.Drawing.Color.Black;
+            this.nameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameButton.ForeColor = System.Drawing.Color.White;
+            this.nameButton.Location = new System.Drawing.Point(236, 106);
+            this.nameButton.Name = "nameButton";
+            this.nameButton.Size = new System.Drawing.Size(157, 34);
+            this.nameButton.TabIndex = 2;
+            this.nameButton.Text = "Set Name";
+            this.nameButton.UseVisualStyleBackColor = false;
+            this.nameButton.Click += new System.EventHandler(this.nameButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(231, 199);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(64, 25);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "label1";
+            this.outputLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.nameButton);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.nameLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hero Quest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Button nameButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Hero Quest/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hero_Quest
+{
+    public partial class Form1 : Form
+    {
+        string heroName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameButton_Click(object sender, EventArgs e)
+        {
+            heroName = nameInput.Text;
+           //this will show the text that appears after you press the button
+            outputLabel.Text = "hello ezra. have a glorious adventure.";
+            outputLabel.Visible = true;
+            //this makes the label visible when the code is ran
+        }
+    }
+}
